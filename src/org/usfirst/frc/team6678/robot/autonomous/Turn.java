da3052c5538f484fcb5fff046d3312424f6d70a4
viktorstrate <viktorstrate@gmail.com>
2017-02-22 14:30:27 +0100

    Tilføjede turn klasse

---
+++ src/org/usfirst/frc/team6678/robot/autonomous/Turn.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team6678.robot.autonomous;
+
+/**
+ * Staar for at dreje robotten autonomt
+ * Created by viktorstrate on 2/22/17.
+ */
+public class Turn implements Autonomous {
+
+    private boolean running;
+    private double degreesToTurn;
+
+    public Turn(double degrees) {
+        this.degreesToTurn = degrees;
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void isRunning() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
