f05e53ab7d79a185efc8b470b6422a68f21671d4
viktorstrate <viktorstrate@gmail.com>
2017-05-31 19:00:39 +0200

    Fixed UltraSonicDistanceSensor.java

--- src/org/usfirst/frc/team6678/robot/backgroundTasks/UltraSonicDistanceSensor.java
+++ src/org/usfirst/frc/team6678/robot/backgroundTasks/UltraSonicDistanceSensor.java
@@ -9,12 +9,12 @@ import org.usfirst.frc.team6678.robot.Log;
 
 /**
  * A class for using the the Ultra Sonic Distance Sensor (LV-MaxSonar® -EZ)
- * Using the RS-232 (Serial) protocol
+ * Using the RS-232 (Serial) protocol.
  */
 public class UltraSonicDistanceSensor extends SensorBase implements BackgroundTask {
 	
 	SerialPort sp;
-	double distance;
+	int distance;
 	boolean hit;
 	
 	public UltraSonicDistanceSensor() {
@@ -24,41 +24,32 @@ public class UltraSonicDistanceSensor extends SensorBase implements BackgroundTa
 
 	@Override
 	public void loop() {
-		//The output is an ASCII capital “R”, followed by three ASCII character digits representing the
-		//range in inches up to a maximum of 255, followed by a carriage return (ASCII 13). 
 		String rawData = sp.readString();
 
-		// *** THIS PART IS NOT TESTED YET ***
-		rawData = rawData.substring(1, 3);
-
-		int inchValue = -1;
+		rawData = rawData.substring(1, 4);
 
 		try {
-			inchValue = Integer.parseInt(rawData);
+			distance = Integer.parseInt(rawData);
 		} catch (NumberFormatException e){
 			Log.error("UltraSonicDistanceSensor", "Could not parse raw data from sensor:");
 			e.printStackTrace();
 		}
 
-		if (inchValue > 0) {
-			hit = inchValue == 255;
-			distance = inchValue * 2.54;
-		}
+		hit = distance == 5000;
 
-		Log.debug("UltraSonicDistanceSensor", "Distance: "+distance+" cm");
 	}
 
 	/**
-	 *
-	 * @return The distance from the Sonic Distance Sensor,
+	 * The distance is a value in millimeters ranging from 300mm as the minimum, and a maximum range up to 5000mm (5m)
+	 * @return The distance from the Sonic Distance Sensor, in millimeters.
 	 */
-	public double getDistance() {
+	public int getDistance() {
 		return distance;
 	}
 
 	/**
-	 *
-	 * @return True if sensor hit an object
+	 * The sensor has a maximum range of 5m. This method returns whether or not the sensor hit somthing or not
+	 * @return True if sensor hit an object, false if not
 	 */
 	public boolean getHit() {
 		return hit;
